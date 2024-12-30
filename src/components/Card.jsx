d6f0bbc81db3f0db5c64436df53383237fd9bb2c
box3101 <box4101@nate.com>
2024-12-30 16:43:26 +0900

    4. 실제 수정 버튼 예시

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,8 +9,16 @@ function Card() {
   const [posts, setPosts] = useState(postData);
 
   // 3. 매개변수가 있는 이벤트 함수 정의
-  const handleClick = (id) => {
-    console.log(`${id}버튼이 클릭되었습니다.`);
+  const handleEdit = (id) => {
+    // posts 배열에서 해당 id의 게시물을 찾아서 수정정
+    const updateEdit = posts.map((post) => {
+      if (post.id === id) {
+        return {...post , title : "title 변경"}
+      }else{
+        return post
+      }
+    });
+    setPosts(updateEdit);
   }
 
   return (
@@ -21,12 +29,11 @@ function Card() {
             <h2>{post.title}</h2>
             <p>{post.content}</p>
           </div>
-          <button>수정</button>
+          <button onClick={() => {
+            handleEdit(post.id)
+          }}>수정</button>
         </article>
       ))}
-
-      {/* 매개 변수가 있는 함수는 () => {} 화살표 함수를 사용 */}
-      <button onClick={()=>{handleClick(1)}}>클릭하세요</button>
     </>
   )
 }
